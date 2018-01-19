7c17b2605b374b76a74f7f5cde5b026384a7facb
Frank Faustino <nawzyah@gmail.com>
2018-01-19 11:26:40 -0800

    Added JavaScript Solution

---
+++ hackerrank/Data Structures/Linked Lists/02 — Insert a Node at the Tail of a Linked List/solution.js
@@ -0,0 +1,26 @@
+// Source : https://www.hackerrank.com/challenges/insert-a-node-at-the-tail-of-a-linked-list/problem
+// Author : Frank Faustino
+// Date   : 2019-01-19
+
+/*
+  Node is defined as
+  var Node = function(data) {
+      this.data = data;
+      this.next = null;
+  }
+*/
+
+// This is a "method-only" submission.
+// You only need to complete this method.
+
+const insert = (head, data) => {
+  if (!head) head = new Node(data)
+  let current = head
+  while (current.next) {
+    current = current.next
+  }
+  current.next = new Node(data)
+  return head
+}
+
+
