626ecbb05dc1548c41a882be6fb9fea0126fc3b4
Frank Faustino <nawzyah@gmail.com>
2018-02-11 23:50:25 -0800

    Added The falling speed of petals 🌸

---
+++ codewars/8 kyu/The falling speed of petals/solution.js
@@ -0,0 +1,6 @@
+// Source : https://www.codewars.com/kata/the-falling-speed-of-petals/javascript
+// Author : Frank Faustino
+// Date   : 2018-02-11
+// Lang   : JavaScript
+
+const sakuraFall = v => v > 0 ? 400 / v : 0
