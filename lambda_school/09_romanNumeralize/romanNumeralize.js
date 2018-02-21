add5165141f240731064841fd281cebcb656a532
Frank Faustino <nawzyah@gmail.com>
2018-02-21 08:58:16 -0800

    Added 09 Roman Numeralize (js)

---
+++ lambda_school/09_romanNumeralize/romanNumeralize.js
@@ -0,0 +1,22 @@
+/* ———
+ * Define a function that takes in a positive integer,
+ * and returns the Roman Numeral representation of that number.
+
+   Symbol    Value
+   I          1
+   V          5
+   X          10
+   L          50
+   C          100
+   D          500
+   M          1,000
+ * ——— */
+
+const romanNumeralize = number => {
+  // Code here
+}
+
+/* ——— Test cases ——— */
+romanNumeralize(11)   // 'XI'
+romanNumeralize(1990) // 'MCMXC'
+romanNumeralize(999)  // 'CMXCIX'
