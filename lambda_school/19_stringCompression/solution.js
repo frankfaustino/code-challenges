d5f61c0ecd9cc63fce8d139b87da25f84815c3f2
Frank Faustino <nawzyah@gmail.com>
2018-03-13 20:56:19 -0700

    19_stringCompression

---
+++ lambda_school/19_stringCompression/solution.js
@@ -0,0 +1,19 @@
+// Title  : String Compression
+// Author : Frank Faustino
+// Date   : 2018-03-18
+// Lang   : JavaScript
+
+const stringCompression = str => {
+  let output = ''
+  let count = 1
+
+  for (let i = 1; i <= str.length; i++) {
+    if (str[i - 1] === str[i]) count++
+    else if (str[i - 1] !== str[i]) {
+      output += str[i - 1] + count
+      count = 1
+    }
+  }
+
+  return output.length < str.length ? output : str
+}
