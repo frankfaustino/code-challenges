7a09e265b49cba71463db2c17078d4734afd6aa3
Frank Faustino <nawzyah@gmail.com>
2018-03-18 23:59:45 -0700

    14_primeList

---
+++ lambda_school/14_primeList/primeList.js
@@ -0,0 +1,11 @@
+/* ———
+ * Write a function that generates a list of all prime numbers in a specified range (inclusive).
+ * If you're not quite sure where to start, check out the Sieve of Eratosthenes on Wikipedia.
+ * ——— */
+
+const primeList = (start, end) => {
+  // Code here
+}
+
+console.log(primeList(100, 150)) // [ 101, 103, 107, 109, 113, 127, 131, 137, 139, 149 ]
+console.log(primeList(1000, 1050)) // [ 1009, 1013, 1019, 1021, 1031, 1033, 1039, 1049 ]
