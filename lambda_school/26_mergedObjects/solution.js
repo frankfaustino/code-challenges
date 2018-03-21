d72cfd190c9988bfbff16db8c154114aa1a69e7e
Frank Faustino <nawzyah@gmail.com>
2018-03-21 11:56:15 -0700

    updated 26_mergedObjects

--- lambda_school/26_mergedObjects/solution.js
+++ lambda_school/26_mergedObjects/solution.js
@@ -3,10 +3,13 @@
 // Date   : 2018-03-21
 // Lang   : JavaScript
 
-// a) reduce, spread syntax
+// a) Object.assign, spread syntax
+const mergeObjects = arr => Object.assign({}, ...arr)
+
+// b) reduce, spread syntax
 const mergeObjects = arr => arr.reduce((a, c) => ({...a, ...c}))
 
-// b) forEach, Object.assign
+// c) forEach, Object.assign
 const mergeObjects = (arr) => {
   const output = {}
   arr.forEach((obj) => {
@@ -15,13 +18,13 @@ const mergeObjects = (arr) => {
   return output
 }
 
-// c) forEach, Object.keys, bracket notation
+// d) forEach, Object.keys
 const mergeObjects = (arr) => {
   let output = {}
 
   arr.forEach(obj => {
     Object.keys(obj).forEach(key => {
-      if (!output[key] || output[key] !== obj[key]) output[key] = obj[key]
+      output[key] = obj[key]
     })
   })
   return output
