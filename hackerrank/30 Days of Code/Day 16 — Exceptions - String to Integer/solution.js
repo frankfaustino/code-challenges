77c6bbe4ce3799bb8007480c1daf9bef20aad0d8
Frank Faustino <nawzyah@gmail.com>
2018-03-30 23:58:53 -0700

    Day 16 — Exceptions - String to Integer

---
+++ hackerrank/30 Days of Code/Day 16 — Exceptions - String to Integer/solution.js
@@ -0,0 +1,13 @@
+// Source : https://www.hackerrank.com/challenges/30-exceptions-string-to-integer/problem
+// Author : Frank Faustino
+// Date   : 2018-03-30
+// Lang   : JavaScript
+
+const main = () => {
+  const S = readLine();
+  try {
+    console.log(parseInt(S).toString().replace('NaN', 'Bad String'))
+  } catch (err) {
+    console.log(err)
+  }
+}
