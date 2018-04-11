0683c75f78fe95d990d330d8e3d46f19305cc52d
Frank Faustino <nawzyah@gmail.com>
2018-04-10 23:59:02 -0700

    33 — Digits Raised to Consecutive Powers

---
+++ lambda_school/33_sumDigitPower/sumDigitPower.js
@@ -0,0 +1,16 @@
+/* ———
+ * The number 135 is particularly special in that if you take each digit, from left to right,
+ * and you put them to the consecutive power.
+ * Write a function sumDigitPower that takes in a starting number and an ending number,
+ * and check every number in that range inclusive (including a and b) for this property.
+ * a will be greater than or equal to 0. Return all these numbers in this range which
+ * satisfies this property as an array.
+ *
+ * Example:
+ * 135 = 1^1 + 3^2 + 5^3
+ * 89 = 8^1 + 9^2
+ * ——— */
+
+const sumDigitPower = (a, b) => {
+  // Code here
+}
