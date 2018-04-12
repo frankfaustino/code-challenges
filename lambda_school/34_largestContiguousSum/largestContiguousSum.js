9a2e10c2498642dd05f9bd2c59d6458695e402d4
Frank Faustino <nawzyah@gmail.com>
2018-04-11 23:57:09 -0700

    34 — Largest Contiguous Sum

---
+++ lambda_school/34_largestContiguousSum/largestContiguousSum.js
@@ -0,0 +1,14 @@
+/* ———
+ * Given an array of numbers containing at least one positive number (or zero),
+ * find the greatest contiguous sum of numbers.
+ * Contiguous, in this case, can be as few as zero numbers
+ * (an empty subset of the original array, with a total worth of zero).
+ * ——— */
+
+const sumArray = arr => {
+  // Code here
+}
+
+sumArray([1, 2, 3])     // => 6
+sumArray([4, -1, 5])    // => 8
+sumArray([10, -11, 11]) // => 11
