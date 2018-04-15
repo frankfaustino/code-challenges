087452e924487abfa64d310afc9ad12940f2da10
Frank Faustino <nawzyah@gmail.com>
2018-04-14 23:59:00 -0700

    35 Prime Reduction

---
+++ lambda_school/35_primeReduction/primeReduction.js
@@ -0,0 +1,14 @@
+/* ———
+ * Consider the prime number 23.
+ * If we sum the square of its digits we get: 2^2 + 3^2 = 13,
+ * then for 13: 1^2 + 3^2 = 10, and finally for 10: 1^2 + 0^2 = 1.
+ * Similarly, if we start with prime number 7, the sequence is: 7-> 49-> 97-> 130-> 10-> 1.
+ * Given a range, how many primes within that range have this property?
+ * ——— */
+
+const primeReduction = (a, b) => {
+  // Code here
+}
+
+primeReduction(2, 8) // 1
+primeReduction(1, 9213) // 193
