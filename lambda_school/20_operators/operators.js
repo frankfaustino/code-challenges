bbbb161aee286ea5670116711e8cfb4e8dcf5ecc
Frank Faustino <nawzyah@gmail.com>
2018-04-17 23:22:56 -0700

    20 Operators

---
+++ lambda_school/20_operators/operators.js
@@ -0,0 +1,18 @@
+/* ———
+ * Implement three functions called multiply, divide, and modulo.
+ * These functions should multiply, divide, or return the remainder of two arguments.
+ * Now for the tricky part: you can only use + and - to implement these functions.
+ * Divide should drop the remainder.
+ * ——— */
+
+const multiply = (x, y) => {
+  // Code here
+}
+
+const divide = (x, y) => {
+  // Code here
+}
+
+const modulo = (x, y) => {
+  // Code here
+}
