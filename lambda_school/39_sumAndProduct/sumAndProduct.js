6a076464c94c0fa646aa4dd90113f612ef6822a2
Frank Faustino <nawzyah@gmail.com>
2018-04-19 23:59:18 -0700

    39 Sum and Product

---
+++ lambda_school/39_sumAndProduct/sumAndProduct.js
@@ -0,0 +1,10 @@
+/* ———
+ * Given a sum and a product, find two integers x and y, where x + y === sum, and x * y === product.
+ * You will return this in an array.
+ * For example, calling sumAndProduct(6, 9) should return [3, 3] because 3 + 3 === 6, and 3 * 3 === 9.
+ * Please make sure to return your array such that x <= y in the format [x, y].
+ * ——— */
+
+const sumAndProduct = (sum, product) => {
+  // Code here
+}
