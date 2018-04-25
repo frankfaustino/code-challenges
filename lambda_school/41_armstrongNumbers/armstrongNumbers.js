b561153ea11a42e6da6feeea1bf8ee89a570cb89
Frank Faustino <nawzyah@gmail.com>
2018-04-24 23:50:00 -0700

    41 Armstrong Numbers

---
+++ lambda_school/41_armstrongNumbers/armstrongNumbers.js
@@ -0,0 +1,15 @@
+/* ———
+ * An Armstrong number is an n-digit number that is equal to the sum of the n'th powers of its digits.
+ * Determine if the input number is an Armstrong number. Return either true or false.
+ *
+ * For example with the number 153 there are 3 digits, so you would add together the cubed values of all
+ * the digits like this: 1^3 + 5^3 + 3^3 === 153
+ * ——— */
+
+const armstrongNumbers = n => {
+  // Code here
+}
+
+armstrongNumbers(6) // true
+armstrongNumbers(153) // true
+armstrongNumbers(351) // false
