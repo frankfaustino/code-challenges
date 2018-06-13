c1620896e97b57cf15e16457787d6ad072f6a9be
Frank Faustino <nawzyah@gmail.com>
2018-06-13 14:52:38 -0700

    Added Disemvowel Trolls

---
+++ codewars/7 kyu/Disemvowel Trolls/solution.js
@@ -0,0 +1,6 @@
+// Source : https://www.codewars.com/kata/disemvowel-trolls/javascript
+// Author : Frank Faustino
+// Date   : 2018-06-13
+// Lang   : JavaScript
+
+const disemvowel = str => str.replace(/[aeiou]/gi, '')
