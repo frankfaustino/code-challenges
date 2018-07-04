498ce89f31bbc988c39175e31846f4cfeb2780b7
Frank Faustino <nawzyah@gmail.com>
2018-07-03 22:47:55 -0700

    Added Shorten Me - Mad Mathematician's Algorithm I (Addition)

---
+++ codewars/7 kyu/Shorten Me - Mad Mathematician's Algorithm I (Addition)/solution.js
@@ -0,0 +1,7 @@
+// Source : https://codewars.com/kata/shorten-me-mad-mathematicians-algorithm-i-addition/
+// Author : Frank Faustino
+// Date   : 2018-07-03
+// Lang   : JavaScript
+
+// binary and unary operators
+madAdd=(a,b)=>a- -b
