19c8e64fb252bc65de2f027afce9f8ba9e32e00e
Frank Faustino <nawzyah@gmail.com>
2018-07-07 16:47:39 -0700

    Added Is it a number?

---
+++ codewars/8 kyu/Is it a number?/solutions.js
@@ -0,0 +1,7 @@
+// Source : https://codewars.com/kata/is-it-a-number
+// Author : Frank Faustino
+// Date   : 2018-07-07
+// Lang   : JavaScript
+
+// parseFloat parses an argument and returns a floating point number.
+const isDigit = s => s == parseFloat(s)
