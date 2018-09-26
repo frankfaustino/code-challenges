1472eb07887f1a4efb2efc659fb45b6379b13970
Frank Faustino <nawzyah@gmail.com>
2018-09-26 14:12:13 -0700

    Fix Largest Numbers in Arrays solution

--- various/FCC — Largest Numbers in Arrays.js
+++ various/FCC — Largest Numbers in Arrays.js
@@ -2,9 +2,8 @@
 // Author : Frank Faustino
 // Date   : 2017-09-12
 
-const largestOfFour = arr => mainArray.map((subArray) => Math.max(...subArray))
+const largestOfFour = arr => arr.map(subArr => Math.max(...subArr))
 
-// const largestOfFour = mainArray => mainArray
-//   .map((subArray) => subArray.reduce((a, b) => (b > a) ? b : a, 0))
+// const largestOfFour = arr => arr.map(subArr => subArr.reduce((a, b) => (b > a) ? b : a, 0))
 
 console.log(largestOfFour([[4, 5, 1, 3], [13, 27, 18, 26], [32, 35, 37, 39], [1000, 1001, 857, 1]]))
