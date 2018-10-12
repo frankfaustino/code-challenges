74f3b05feda830bc642e91b21c33166ecb929b93
Frank Faustino <nawzyah@gmail.com>
2018-10-11 19:23:13 -0700

    Add Reverse Linked List unit test

---
+++ src/lambda_school/45. Reverse Linked List/__tests__/reverseLinkedList.test.py
@@ -0,0 +1,66 @@
+from os.path import abspath, dirname, join
+import sys
+import unittest
+
+sys.path.insert(1, abspath(join(dirname(__file__), '..')))
+
+from solution import reverseList
+
+class Test(unittest.TestCase):
+
+    class LinkedListNode(object):
+
+        def __init__(self, value, next=None):
+            self.value = value
+            self.next  = next
+
+        def get_values(self):
+            node = self
+            values = []
+            while node is not None:
+                values.append(node.value)
+                node = node.next
+            return values
+
+    def test_short_linked_list(self):
+        second = Test.LinkedListNode(2)
+        first = Test.LinkedListNode(1, second)
+
+        result = reverseList(first)
+        self.assertIsNotNone(result)
+
+        actual = result.get_values()
+        expected = [2, 1]
+        self.assertEqual(actual, expected)
+
+    def test_long_linked_list(self):
+        sixth = Test.LinkedListNode(6)
+        fifth = Test.LinkedListNode(5, sixth)
+        fourth = Test.LinkedListNode(4, fifth)
+        third = Test.LinkedListNode(3, fourth)
+        second = Test.LinkedListNode(2, third)
+        first = Test.LinkedListNode(1, second)
+
+        result = reverseList(first)
+        self.assertIsNotNone(result)
+
+        actual = result.get_values()
+        expected = [6, 5, 4, 3, 2, 1]
+        self.assertEqual(actual, expected)
+
+    def test_one_element_linked_list(self):
+        first = Test.LinkedListNode(1)
+
+        result = reverseList(first)
+        self.assertIsNotNone(result)
+
+        actual = result.get_values()
+        expected = [1]
+        self.assertEqual(actual, expected)
+
+    def test_empty_linked_list(self):
+        result = reverseList(None)
+        self.assertIsNone(result)
+
+
+unittest.main(verbosity=2)
