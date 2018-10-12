ceb6513017157c6b8cc0bcd93bd589e43dc7f201
Frank Faustino <nawzyah@gmail.com>
2018-10-11 19:22:58 -0700

    Update Reverse Linked List python solution to in-place solution

--- src/lambda_school/45. Reverse Linked List/solution.py
+++ src/lambda_school/45. Reverse Linked List/solution.py
@@ -13,41 +13,31 @@ class Node:
     return f'{self.value}'
 
 # Time Complexity: O(n)
-# Space Complexity: O(n)
-# Can do better than this. What about in-place?
-def reverseList(self):
-  stack = []
-  head = None
+# Space Complexity: O(1)
+def reverseList(node):
   prev = None
+  next = None
 
-  for i in self:
-    stack.append(i)
-
-  while len(stack) > 0:
-    node = stack.pop()
-    if prev:
-      prev.next = node
-      if len(stack) is 0:
-        node.next = None
-        break
-    else:
-      head = node
+  while node:
+    next = node.next
+    node.next = prev
     prev = node
+    node = next
 
-  return head
+  return prev
 
-NodeA = Node('A')
-NodeB = Node('B')
-NodeC = Node('C')
-NodeD = Node('D')
+# NodeA = Node('A')
+# NodeB = Node('B')
+# NodeC = Node('C')
+# NodeD = Node('D')
 
-NodeA.next = NodeB
-NodeB.next = NodeC
-NodeC.next = NodeD
+# NodeA.next = NodeB
+# NodeB.next = NodeC
+# NodeC.next = NodeD
 
-reversed = reverseList(NodeA)
-print(' ➜ '.join([str(node) for node in reversed]))
+# reversed = reverseList(NodeA)
+# print(' ➜ '.join([str(node) for node in reversed]))
 
-print(NodeD.next) # NodeC
-print(NodeC.next) # NodeB
-print(NodeB.next) # NodeA
+# print(NodeD.next) # NodeC
+# print(NodeC.next) # NodeB
+# print(NodeB.next) # NodeA
