dc41c002a5c5637a349aac59cfb6ba0046217e3f
Frank Faustino <nawzyah@gmail.com>
2019-01-06 17:38:00 -0800

    Add Strip Comments

---
+++ src/codewars/4 kyu/Strip Comments/stripComments.js
@@ -0,0 +1,19 @@
+// Snail  : https://www.codewars.com/kata/strip-comments/train/javascript
+// Author : Frank Faustino
+// Date   : 2019-01-06
+
+function solution(input, markers) {
+  markers.forEach(marker => {
+    let index = input.indexOf(marker)
+    if (index > -1) {
+      if (input[index - 1] === ' ') {
+        index--
+      }
+      const linebreak = input.indexOf('\n', index)
+      input = linebreak > 0 ? (input.slice(0, index) + input.slice(linebreak)) : input.slice(0, index)
+    }
+  })
+  return input
+}
+
+module.exports = solution
