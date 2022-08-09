e8c45af915845b401bc4e7b0f597642138760af4
Frank Faustino <frank.faustino@gmail.com>
2022-08-08 23:45:13 -0700

    Add Ransom Note optimal solution

--- src/leetcode/0383 Ransom Note/solution.js
+++ src/leetcode/0383 Ransom Note/solution.js
@@ -1,11 +1,44 @@
 /**
+ * Time complexity: O(m) [m >= n]
+ * Space complexity: O(k) or O(1) [k is at most 26]
+ * @param {string} ransomNote
+ * @param {string} magazine
+ * @return {boolean}
+ */
+ function canConstruct(ransomNote, magazine) {
+    // Approach:
+    // - count the frequency of each character in magazine
+    // - store the counts in a hash map
+    // - iterate through the chars in ransomNote
+    // - subtract the count from magazineCount hash map
+    // - if we come across 0, return false
+    const magazineCount = makeFrequencyMap(magazine)
+        
+    for (const char of ransomNote) {
+        if (!magazineCount[char]) return false
+
+        magazineCount[char]--
+    }
+    return true
+}
+
+const makeFrequencyMap = string =>
+    string
+        .split('')
+        .reduce((acc, cur) => {
+            if (acc[cur] === undefined) acc[cur] = 1
+            else acc[cur]++
+            return acc
+        }, {})
+
+/**
  * Time complexity: 3⋅O(m) or O(m)
  * Space complexity: O(k) or O(1) [k is at most 26]
  * @param {string} ransomNote
  * @param {string} magazine
  * @return {boolean}
  */
-function canConstruct(ransomNote, magazine) {
+function canConstructAlternative(ransomNote, magazine) {
     // Approach:
     // - count the frequency of each character in ransomNote
     // - count the frequency of each character in magazine
@@ -25,15 +58,6 @@ function canConstruct(ransomNote, magazine) {
     }
     return true
 }
-
-const makeFrequencyMap = string =>
-    string
-        .split('')
-        .reduce((acc, cur) => {
-            if (acc[cur] === undefined) acc[cur] = 1
-            else acc[cur]++
-            return acc
-        }, {})
 
 console.log(canConstruct("a", "b")) // false
 console.log(canConstruct("aa", "ab")) // false
