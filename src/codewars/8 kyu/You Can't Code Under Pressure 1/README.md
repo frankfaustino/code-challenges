fd24ef965275ab601971b0ff8a6e0b837916c9d7
Frank Faustino <frank.faustino@gmail.com>
2022-08-30 12:44:23 -0700

    Add You Can't Code Under Pressure 1

---
+++ src/codewars/8 kyu/You Can't Code Under Pressure 1/README.md
@@ -0,0 +1,4 @@
+# You Can't Code Under Pressure #1
+
+Code as fast as you can! You need to double the integer and return it.
+
---
+++ src/codewars/8 kyu/You Can't Code Under Pressure 1/main.rs
@@ -0,0 +1,11 @@
+fn double_integer(n: i32) -> i32 {
+    n * 2
+}
+
+fn main() {
+    double_integer(1); // 2
+    double_integer(5); // 10
+    double_integer(10); // 20
+    double_integer(20); // 40
+    double_integer(40); // 80
+}
