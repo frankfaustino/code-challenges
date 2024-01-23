5ed2acb9b376164fec8978f8fa79393e3b8798b2
Frank Faustino <frank.faustino@gmail.com>
2024-01-22 19:13:03 -0800

    Update Moving Zeros To The End

--- src/codewars/5 kyu/Moving Zeros To The End/solution.js
+++ src/codewars/5 kyu/Moving Zeros To The End/solution.js
@@ -2,15 +2,15 @@
 // Author : Frank Faustino
 // Date   : 2024-01-22
 
+// Time complexity  : O(n log n)
+// Space complexity : O(1)
+// const moveZeros = arr => arr.sort((_, e) => e === 0 ? -1 : 0)
+
 // Time complexity  : O(n)
 // Space complexity : O(n)
 function moveZeros(arr) {
-    const left = []
-    const right = []
-
-    arr.forEach(e => e === 0 ? right.push(e) : left.push(e))
-
-    return [...left, ...right]
+    const left = arr.filter(e => e !== 0)
+    return [...left, ...Array(arr.length - left.length).fill(0)]
 }
 
 module.exports = moveZeros
